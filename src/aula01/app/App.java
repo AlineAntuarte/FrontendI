8ebbb3e20f388663818d2fb4a10b34ba48a7b51a
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-02-12 19:26:10 -0300

    Organização

--- src/aula01/app/App.java
+++ src/aula01/app/App.java
@@ -1,3 +1,4 @@
+package aula01.app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/aula01/modelos/classeExemplo.java
@@ -0,0 +1,5 @@
+package aula01.modelos;
+
+public class classeExemplo {
+    
+}
